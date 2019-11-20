4dc5695cf395fc87129876777242903145afedf4
Patric B <pb@vierbeuter.de>
2019-11-20 14:20:06 +0100

    data structure and mobile styling

--- myApp/src/pages/Product.tsx
+++ myApp/src/pages/Product.tsx
@@ -1,8 +1,9 @@
-import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonIcon, IonGrid, IonRow, IonCol, IonSlides, IonSlide, IonImg, IonLoading} from '@ionic/react';
+import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonIcon, IonGrid, IonRow, IonCol, IonSlides, IonSlide, IonImg, IonLoading, IonSelect, IonSelectOption, IonLabel, IonItem} from '@ionic/react';
 import React, { Component } from 'react';
 
 type ProductState = {
 	loaded: boolean,
+	currency: string,
 	product: [{ [key: string]: any[] }];
 }
 
@@ -10,12 +11,13 @@ class Product extends Component<{}, ProductState> {
 
 	public readonly state: Readonly<ProductState> = {
 		loaded: false,
+		currency: "€",
 		product: [{}]
 	};
 
 	componentDidMount(): void {
 		const slug = window.location.pathname.split('/')[2];
-		const query = `query { entries(section:"product",slug:"${slug}"){ title, ... on product_product_Entry {ingredients,priceWithTax,priceWithoutTax,productDescription,productImages{ url },productName,variations}}}`;
+		const query = `query { entries(section:"product",slug:"${slug}"){ title, ... on product_product_Entry {ingredients,priceWithTax,priceWithoutTax,productDescription,productImages{ url },productName,variations}}, globalSets { ... on shopSettings_GlobalSet { currency }}}`;
 		const url = "http://local.ionic-project.de/api";
 		const opts = {
 			method: "POST",
@@ -24,12 +26,11 @@ class Product extends Component<{}, ProductState> {
 		};
 		fetch(url, opts)
 			.then(res => res.json())
-			.then(res => this.setState({product: res.data.entries[0]}))
+			.then(res => this.setState({product: res.data.entries[0], currency: res.data["globalSets"][0]['currency']}))
 			.then(() => this.setState({loaded: true}))
 	}
 
 	render() {
-		console.log(this.state.product);
 		const slideOpts = {
 			initialSlide: 1,
 			speed: 400
@@ -42,11 +43,11 @@ class Product extends Component<{}, ProductState> {
 				</IonToolbar>
 			</IonHeader>
 			<IonContent className="ion-padding">
-				<h2>{this.state.product['title']}</h2>
-				<IonGrid>
-					<IonRow>
-						<IonCol size={"12"}>
-							{this.state.loaded ?
+				{ this.state.loaded ?
+				 <div>
+					<IonGrid>
+						<IonRow>
+							<IonCol size={"12"} sizeMd={"6"} pushMd={"1"}>
 								<IonSlides pager={true} options={slideOpts}>
 									{this.state.product['productImages'].map( (image:object, index:number) => {
 										return (
@@ -56,16 +57,40 @@ class Product extends Component<{}, ProductState> {
 										)
 									})}
 								</IonSlides>
-							: <IonLoading
-								isOpen={!this.state.loaded}
-								message={'Loading data...'}
-								/>}
-						</IonCol>
-						<IonCol dangerouslySetInnerHTML={this.state.product['productDescription']} />
-					</IonRow>
-				</IonGrid>
+							</IonCol>
+							<IonCol size={"12"} sizeMd={"6"} pushMd={"1"}>
+								<h2>{this.state.product['title']}</h2>
+								<div>
+									<h2>{this.state.product['priceWithTax'] + ' ' + this.state.currency}</h2>
+									<p>{this.state.product['priceWithoutTax'] + ' ' + this.state.currency } Wholesale price</p>
+								</div>
+								<IonItem>
+									<IonLabel>Variations</IonLabel>
+									<IonSelect multiple={false} cancelText="Cancel" okText="Choose">
+										{this.state.product['variations'].split(',').map( (variation:string, index:number) => {
+											return (
+												<IonSelectOption key={index}>
+													{variation}
+												</IonSelectOption>
+											)
+										})}
+									</IonSelect>
+								</IonItem>
+							</IonCol>
+						</IonRow>
+						<IonRow>
+							<IonCol size={"12"} sizeMd={"8"} pushMd={"2"} >
+								{this.state.product['productDescription']}
+							</IonCol>
+						</IonRow>
+					</IonGrid>
+				</div>
+				: <IonLoading
+					isOpen={!this.state.loaded}
+					message={'Loading data...'}
+				/>}
 			</IonContent>
-		</IonPage>;
+		</IonPage>
 	}
 }
 
